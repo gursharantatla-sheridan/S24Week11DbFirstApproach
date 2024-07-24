0ad099af1eb208334e9931f66a1f1234224c3960
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-24 09:45:06 -0400

    insert student

--- S24Week11DbFirstApproach/MainWindow.xaml.cs
+++ S24Week11DbFirstApproach/MainWindow.xaml.cs
@@ -65,5 +65,18 @@ namespace S24Week11DbFirstApproach
                 MessageBox.Show("Invalid ID. Please try again.");
             }
         }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Student std = new Student();
+            std.StudentName = txtName.Text;
+            std.StandardId = (int)cmbStandard.SelectedValue;
+
+            db.Students.Add(std);
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("New student added");
+        }
     }
 }
